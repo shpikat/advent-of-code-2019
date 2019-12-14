7e23bc16e319b054973378b523a1a2ec0f00165d
Alexei Shpikat <a.shpikat@gmail.com>
2019-12-14 13:42:39 +0300

    Solve Day 10

--- src/com/shpikat/adventofcode2019/day10/Main.java
+++ src/com/shpikat/adventofcode2019/day10/Main.java
@@ -7,7 +7,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.BitSet;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static java.lang.Math.PI;
 
 public class Main {
 
@@ -28,6 +35,8 @@ public class Main {
         }
 
         int max = 0;
+        int bestX = 0;
+        int bestY = 0;
         for (int candidate = sourceMap.nextSetBit(0); candidate >= 0; candidate = sourceMap.nextSetBit(candidate + 1)) {
             final int x1 = candidate % width;
             final int y1 = candidate / width;
@@ -66,16 +75,71 @@ public class Main {
                 }
             }
 
-            max = Math.max(max, mapCopy.cardinality());
+            final int current = mapCopy.cardinality();
+            if (current > max) {
+                max = current;
+                bestX = x1;
+                bestY = y1;
+            }
             if (candidate == Integer.MAX_VALUE) {
                 break; // or (candidate+1) would overflow
             }
-
         }
-        System.out.println(max);
+
+        // Prepare the laser!
+        sourceMap.clear(bestY * width + bestX);
+        final SortedMap<Double, Queue<Asteroid>> thetas = new TreeMap<>();
+        for (int asteroid = sourceMap.nextSetBit(0); asteroid >= 0; asteroid = sourceMap.nextSetBit(asteroid + 1)) {
+            final int x = asteroid % width;
+            final int y = asteroid / width;
+
+            final int deltaX = bestX - x;
+            final int deltaY = bestY - y;
+
+            final double theta = adjustStartingPosition(Math.atan2(deltaY, deltaX));
+
+            thetas.computeIfAbsent(theta, key -> new PriorityQueue<>(Asteroid.byDistance))
+                    .add(new Asteroid(x, y, deltaX * deltaX + deltaY * deltaY));
+        }
+
+        int count = 0;
+        final int limit = 200;
+        do {
+            for (final Queue<Asteroid> asteroids : thetas.values()) {
+                final Asteroid asteroid = asteroids.poll();
+                if (asteroid != null && ++count == limit) {
+                    System.out.println(asteroid.x * 100 + asteroid.y);
+                    break;
+                }
+
+            }
+        } while (count < limit);
+
     }
 
     private static int getGcd(final int n1, final int n2) {
         return n2 == 0 ? n1 : getGcd(n2, n1 % n2);
+    }
+
+    private static double adjustStartingPosition(final double angle) {
+        return angle >= 0.5 * PI ? angle - 2 * PI : angle;
+    }
+
+    private static class Asteroid {
+        private static final Comparator<Asteroid> byDistance = Comparator.comparing(Asteroid::getDistanceSquared);
+
+        private final int x;
+        private final int y;
+        private final int distanceSquared;
+
+        private Asteroid(final int x, final int y, final int distanceSquared) {
+            this.x = x;
+            this.y = y;
+            this.distanceSquared = distanceSquared;
+        }
+
+        private int getDistanceSquared() {
+            return distanceSquared;
+        }
     }
 }
